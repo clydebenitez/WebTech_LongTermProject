217e0573b4a466738b8d97ddf079884ff52e53d6
seanyboyswag <2150218@slu.edu.ph>
2017-03-20 23:01:12 +0800

    Fixed home page and finalized prelim page

--- scripts/script.js
+++ scripts/script.js
@@ -52,7 +52,7 @@ $(document).ready(function(){
   });
 });
 
-// $('body').prepend('<a href="#" class="back-to-top">Back to Top</a>');
+$('body').prepend('<a href="#" class="back-to-top">Back to Top</a>');
 
  // Show/hide contents of topic
 $(document).ready(function(){
@@ -61,3 +61,18 @@ $(document).ready(function(){
     });
 });
 
+var amountScrolled = 300;
+
+$(window).scroll(function() {
+	if ( $(window).scrollTop() > amountScrolled ) {
+		$('a.back-to-top').fadeIn('slow');
+	} else {
+		$('a.back-to-top').fadeOut('slow');
+	}
+});
+$('a.back-to-top').click(function() {
+	$('html, body').animate({
+		scrollTop: 0
+	}, 700);
+	return false;
+});
